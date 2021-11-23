fe686c541f74187d4ddb224a072785dd7a274e11
koray koygun <Koraykoygun@bitbucket.org>
2021-11-23 10:24:24 +0300

    Initial commit

---
+++ src/VFMain.java
@@ -0,0 +1,11 @@
+public class VFMain {
+
+        public static void main(String[] args) {
+            int faktor=1;
+            for(int i = 1; i<= 5; i++){
+                faktor = faktor * i;
+            }
+            System.out.println("Sonuç : "+faktor);
+        }
+
+}
